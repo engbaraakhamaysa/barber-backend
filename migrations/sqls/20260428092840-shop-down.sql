279ef38fca845f23b9125f999dd6fc10bc7f5959
Baraa <engbaraakhamaysa@gmail.com>
2026-05-05 23:56:32 +0300

    remove all file and start build step by step

--- migrations/sqls/20260428092840-shop-down.sql
+++
@@ -1,5 +0,0 @@
-/* Replace with your SQL commands */
-
--- DROP TABLE shops;
-
-DROP TABLE IF EXISTS shops;
--- migrations/sqls/20260428093519-customer-up.sql
+++
@@ -1,9 +0,0 @@
-/* Replace with your SQL commands */
-
-
-CREATE TABLE customers (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) DEFAULT 'customer',
-    phone VARCHAR(50),
-    created_at TIMESTAMP DEFAULT NOW()
-);
